73363fda468b8f620668beba088ed5c58b93ef68
Mohamed Raafat <mohamedraafat060@gmail.com>
2020-04-17 23:43:24 +0200

    minor loop bug

--- components/List.vue
+++ components/List.vue
@@ -59,7 +59,7 @@ export default {
         const res2 = await axios.get(idLink);
         let thumbs = res1.data.items;
         let ids = res2.data.items;
-        for (let i = 1; i < 10; i++) {
+        for (let i = 0; i < 10; i++) {
           let temp = { thumbnail: "", id: "" };
           const thumb = thumbs[i];
           const id = ids[i];
